fd674ce364c6e1f04d1db042a9879c8989ac49ba
deepakmp444 <deepakmp444@gmail.com>
2022-07-29 22:02:59 +0530

    combine two reducers

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const redux = require("redux");
 const createStore = redux.legacy_createStore;
 const bindActionCreators = redux.bindActionCreators;
+const combineReducers = redux.combineReducers;
+
+// Cake
 const CAKE_ORDERED = "CAKE_ORDERED";
 const CAKE_RESTORED = "CAKE_RESTORED";
 
@@ -18,11 +21,11 @@ function restoredCake(qty = 1) {
   };
 }
 
-const initialState = {
+const initialStateOfCake = {
   numberOfCake: 10,
 };
 
-const reducer = (state = initialState, action) => {
+const cakeReducer = (state = initialStateOfCake, action) => {
   switch (action.type) {
     case CAKE_ORDERED:
       return {
@@ -39,17 +42,69 @@ const reducer = (state = initialState, action) => {
   }
 };
 
+// Ice
+const ICE_ORDERED = "ICE_ORDERED";
+const ICE_RESTORED = "ICE_RESTORED";
+
+function orderedIce() {
+  return {
+    type: ICE_ORDERED,
+    quantity: 1,
+  };
+}
+
+function restoredIce(qty = 1) {
+  return {
+    type: ICE_RESTORED,
+    payload: qty,
+  };
+}
+
+const initialStateOfIce = {
+  numberOfIce: 20,
+};
+
+const iceReducer = (state = initialStateOfIce, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numberOfIce: state.numberOfIce - action.quantity,
+      };
+    case CAKE_RESTORED:
+      return {
+        ...state,
+        numberOfIce: state.numberOfIce + action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+// combine two reducers
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  ice: iceReducer,
+});
+
 // const store = createStore(counter)
-const store = createStore(reducer);
+const store = createStore(rootReducer);
 console.log("Initial State ", store.getState());
 const unsubscribe = store.subscribe(() =>
   console.log("Updating Store", store.getState())
 );
 
 // when you use bindActionCreators then you don't use store.dispatch(orderedCake());
-const actions = bindActionCreators({orderedCake, restoredCake}, store.dispatch)
-actions.orderedCake()
-actions.restoredCake(3)
+const actions = bindActionCreators(
+  { orderedCake, restoredCake, orderedIce, restoredIce },
+  store.dispatch
+);
+
+actions.orderedCake();
+actions.restoredCake(3);
+
+actions.orderedIce();
+actions.restoredIce(3);
 
 // store.dispatch(orderedCake());
 // store.dispatch(orderedCake());
@@ -57,4 +112,3 @@ actions.restoredCake(3)
 // store.dispatch(restoredCake(3));
 
 unsubscribe();
-
